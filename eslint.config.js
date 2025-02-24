28e6f3a694495a0c762677505a0790e74b66d785
Leonardo Dantas <leonardo.dantas@wisereducacao.com>
2025-02-24 17:55:09 -0400

    chore: initial configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+
+  // Configurações recomendadas do ESLint, TypeScript, React Query e Jest
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  ...pluginQuery.configs["flat/recommended"],
+
+  // Regras personalizadas
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "no-unused-vars": "off",
+      "no-undef": "error",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
-)
+
+  // Ignorar arquivos/diretórios específicos
+  {
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "coverage/**",
+      "*.config.js",
+      "*.config.ts",
+      "*.d.ts",
+      "src/monitoring/**",
+      "src/**/*.spec.tsx",
+    ],
+  },
+];
